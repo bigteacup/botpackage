b7069f05e1c043b956eb3234d7752b2d639f4960
cassiopee <coatim@gmail.com>
2017-07-10 20:58:47 +0200

    divers ajout de lecture/ecriture/listage de fichiers

--- GestionnaireDeComptes.java
+++ GestionnaireDeComptes.java
@@ -1,41 +1,62 @@
 package botpackage;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class GestionnaireDeComptes {
 
-	
+
+
+	public GestionnaireDeComptes() {
+		
+		 creerDossier();
+	}
 
 	//TODO develloper le chargeur de compte
 	//TODO creer un dossier sil n'existe pas 
 	//charger les configs des comptes
-	public Properties loadConfigComptes(){
+	public void creerDossier(){
+		String userPath = System.getProperty("user.home")+"\\botpackage\\comptes";
+		File dossierComptes = new File(userPath);
+		boolean isCreated = dossierComptes.mkdirs();
+		System.out.println(" Création du dossier"+ userPath +" : " + isCreated);
+		listerComptes();
 		
-	String path = System.getProperty("user.home")+"\\travianfx.properties";		
-	Properties properties = new Properties();
-	FileInputStream input = null;
 
-	try {
-		input = new FileInputStream(path);
-	} catch (FileNotFoundException e) {e.printStackTrace();} 
-
-	try{
-	properties.load(input);
-
-	} catch (IOException e) {e.printStackTrace();}
-	finally{
-		try {
-			input.close();
-		} catch (IOException e) {e.printStackTrace();}
-	}
-	return properties;	
-		
-	}
-
-
+		//établir le dossier 
 	
+	
+	}
+
+public ArrayList<File> listerComptes(){
+	String userPath = System.getProperty("user.home")+"\\botpackage\\comptes";
+	 File dossierComptes = new File(userPath);
+	 File[] list = dossierComptes.listFiles();
+	 ArrayList<File> listComptes = new ArrayList<>();
+	 listComptes.clear();
+	  for(File t : list ){
+		        if (t.getName().toLowerCase().endsWith("properties"))
+		        {
+		        listComptes.add(t);
+		        System.out.println(t.toString());
+		        }
+	
+	 
+}
+	return listComptes;
+
+
+	 }
+	
+public void creerFichierCompte(){
+	
+}
 	
 }
